037f2570f13967b1743502d402331e29ddb98ae6
Adam A.G. Shamblin <signal9@zeroecks.com>
2020-06-15 21:11:30 -0600

    Add hit counter and guestbook

--- vexing_gopher/run_server.py
+++ vexing_gopher/run_server.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-
 '''
 '''
 
+import sqlite3
 import argparse
-from flask import Flask
+from flask import Flask, request
 from flask_gopher import GopherExtension, GopherRequestHandler
 
 
@@ -15,7 +15,19 @@ gopher = GopherExtension(app)
 
 @app.route('/')
 def index():
-    return gopher.render_menu_template('index.gopher')
+    with sqlite3.connect('vexing.db') as conn:
+        cur = conn.cursor()
+
+        cur.execute('''
+        INSERT INTO hits (remote_ip) VALUES (?);
+        ''', (request.environ['REMOTE_ADDR'],))
+
+        cur.execute('''
+        SELECT COUNT(remote_ip) FROM hits;
+        ''')
+        (count,) = cur.fetchone()
+
+    return gopher.render_menu_template('index.gopher', count=count)
 
 
 @app.route('/about')
@@ -23,11 +35,36 @@ def about():
     return gopher.render_menu_template('about.gopher')
 
 
+@app.route('/guestbook')
+def guestbook():
+    message = request.environ.get('SEARCH_TEXT', None)
+    remote_ip = request.environ.get('REMOTE_ADDR', None)
+
+    with sqlite3.connect('vexing.db') as conn:
+        cur = conn.cursor()
+
+        if message != '':
+            cur.execute('''
+            INSERT INTO guestbook (message, remote_ip)
+            VALUES (?, ?);
+            ''', (message[:250], remote_ip))
+
+        cur.execute('''
+        SELECT message, remote_ip, submitted
+        FROM guestbook
+        ORDER BY submitted DESC;
+        ''')
+
+        messages = cur.fetchall()
+
+    form_url = gopher.url_for('guestbook')
+    return gopher.render_menu_template('guestbook.gopher',
+                                       url=form_url,
+                                       messages=messages)
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--config',
-                        help='config file',
-                        default='/etc/vexing-gopher.ini')
     parser.add_argument('-H', '--host',
                         default='0.0.0.0')
     parser.add_argument('-p', '--port',
@@ -37,6 +74,20 @@ def main():
     parser.add_argument('-t', '--threaded',
                         type=bool, default=True)
     args = parser.parse_args()
+
+    with sqlite3.connect('vexing.db') as conn:
+        conn.executescript('''
+        CREATE TABLE IF NOT EXISTS hits
+        (id INTEGER PRIMARY KEY ASC,
+         remote_ip TEXT,
+         accessed TEXT DEFAULT CURRENT_TIMESTAMP);
+
+        CREATE TABLE IF NOT EXISTS guestbook
+        (id INTEGER PRIMARY KEY ASC,
+         remote_ip TEXT,
+         message TEXT,
+         submitted TEXT DEFAULT CURRENT_TIMESTAMP);
+        ''')
 
     app.run(host=args.host,
             port=args.port,
