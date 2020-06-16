eb15ac92cb7273763cc37a8511be1cd1fd440b62
Adam A.G. Shamblin <signal9@zeroecks.com>
2020-06-15 21:23:43 -0600

    Up the version, start on a README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Vexing Workshop Gopher Server
+# Vexing Workshop Gopher Server
+
+## Develop
+
+In a Python3 virtual environment, 
+
+```shell
+python setup.py develop
+```
+
+## Building
+
+```shell
+python setup.py bdist_wheel
+```
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 
 setup(name='vexing_gopher',
-      version='0.0.1',
+      version='0.0.2',
       description='Gopher server for DEFCONMeshnet',
       long_description=long_description,
       url='https://github.com/coyote240/vexing-gopher',
